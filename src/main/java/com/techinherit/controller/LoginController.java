291b71a6a14c8feacf4e12dd824f9c64d8bc0019
Suroj Dangol <surojsmiling@gmail.com>
2018-10-02 16:33:55 +0545

    Add Login Controller

---
+++ src/main/java/com/techinherit/controller/LoginController.java
@@ -0,0 +1,60 @@
+package com.techinherit.controller;
+
+import com.techinherit.dto.LoginDto;
+import com.techinherit.enums.ErrorCode;
+import com.techinherit.utils.DeviceVerificationUtil;
+import com.techinherit.response.MyException;
+import com.techinherit.response.impl.ServiceResponseImpl;
+import java.util.Map;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class LoginController {
+
+    @Autowired
+    private DeviceVerificationUtil deviceVerificationUtil;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("auth")
+    public ServiceResponseImpl<String> createAuthenticationToken(@RequestBody @Valid LoginDto dto, BindingResult result) throws Exception {
+        ServiceResponseImpl<String> response = new ServiceResponseImpl<>();
+        if (result.hasErrors()) {
+            Map<String, String> errors = result.getFieldErrors().stream()
+                    .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+            throw new MyException(ErrorCode.V001, errors);
+        } else {
+            authencate(dto.getUsername(), dto.getPassword());
+            String AuthToken = deviceVerificationUtil.generateToken(dto.getUsername(), dto.getPassword());
+            response.setData(AuthToken);
+            return response;
+        }
+    }
+
+    public boolean authencate(String username, String password) throws UsernameNotFoundException, AuthenticationException {
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                username,
+                password
+        );
+        // Perform the security
+        final Authentication authentication = authenticationManager.authenticate(auth);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return true;
+    }
+}
