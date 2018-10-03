6ee6ac1cbf460973037aab9b47533478719e30b3
Suroj Dangol <surojsmiling@gmail.com>
2018-10-03 12:17:44 +0545

    remove wrong mapper

--- src/main/java/com/techinherit/language/mapper/LanguageMapper.java
+++ src/main/java/com/techinherit/language/mapper/LanguageMapper.java
@@ -1,10 +1,7 @@
 package com.techinherit.language.mapper;
 
-import com.techinherit.basic.dto.UserResponseDto;
-import com.techinherit.basic.entity.Users;
 import com.techinherit.language.dto.LanguageDto;
 import com.techinherit.language.model.Language;
-import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
@@ -15,8 +12,6 @@ public interface LanguageMapper {
     LanguageMapper INSTANCE = Mappers.getMapper(LanguageMapper.class);
 
     Language toLanguage(LanguageDto dto);
-
-    List<UserResponseDto> toUserResponseList(List<Users> page);
 
     void toLanguage(@MappingTarget Language lang, LanguageDto dto);
 }
